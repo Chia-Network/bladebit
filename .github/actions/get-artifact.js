171582310f4ed78d4fffe4e022e170be99835c2e
Harold Brenes <h.brenes@chia.net>
2021-10-05 21:41:28 -0500

    Work

--- .github/actions/get-artifact.js
+++ .github/actions/get-artifact.js
@@ -1,14 +1,58 @@
 const { Octokit } = require( '@octokit/action' );
+const { FS      } = require( 'fs' );
+const log = console.log;
+
+const API = '/repos/Chia-Network/bladebit/actions/artifacts';
+
+// function getArtifactName( os, arch, version )
+// {
+//     const ext = os === 'windows' ? 'zip' : 'tar.gz';
+//     return `bladebit-v${version}-${os}-${arch}.${ext}`;
+// }
+
+// async function uploadArtifact( artifacts, os, arch, version )
+// {
+
+// }
 
 async function main()
 {
+    // const argv = process.args.slice( 2 );
+    // const opts = JSON.parse( argv[0] );
+
     const octokit = new Octokit();
-    const artifacts = await octokit.request( 'GET /repos/{owner}/{repo}/actions/artifacts', {
-        owner: 'Chia-Network',
-        repo : 'bladebit'
-    });
     
-    console.log( JSON.stringify( artifacts, 4 ) );
+    log( `Retrieving artifacts` );
+    let response = await octokit.request( `GET ${API}` );
+    
+    if( response.status !== 200 )
+        throw new Error( 'Invalid artifacts response.' );
+    
+    log( `Looking for artifact for version ${opts.version}` );
+    
+    const artifacts = response.data.artifacts;
+    log( JSON.stringify( artifacts, 4 ) );
+
+    const artifactName = 'bladebit-v1.2.0-ubuntu-x86-64.tar.gz'// getArtifactName( opts.os, opts.version, opts.arch );
+    const artifact     = artifacts.find( a => a.name === artifactName );
+
+    if( !artifact )
+        throw new Error( `Failed to find an artifact name '${artifactName}'.` );
+
+    if( artifact.expired )
+        throw new Error( `Artifact '${artifactName}' has expired.` );
+    
+    log( `Downloading ${artifactName}` );
+    response = await octokit.request( `GET ${API}/{artifact_id}/{archive_format}`, {
+        artifact_id   : artifact.id,
+        archive_format: 'archive_format'
+    });
+
+    if( response !== 302 )
+        throw new Error( `Failed to download artifact.` );
+    
+    // const artifactUrl = artifact.archive_download_url;
+    // FS.
 }
 
 main().then( () => {} );
