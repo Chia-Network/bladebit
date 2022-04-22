743b1fc176e02e28b96d19b686cf95b866422595
Harold Brenes <h.brenes@chia.net>
2022-04-21 21:11:16 -0500

    Variant

---
+++ src/plotdisk/DiskPlotBounded.cpp
@@ -0,0 +1,18 @@
+#include "DiskPlotBounded.h"
+
+template<uint32 _numBuckets>
+class K32BoundedDiskPlotPhase1
+{
+    static constexpr uint32 _k = 32;
+
+    K32BoundedDiskPlotPhase1();
+    ~K32BoundedDiskPlotPhase1();
+
+    void Run();
+
+    //-----------------------------------------------------------
+    void F1()
+    {
+
+    }
+};
