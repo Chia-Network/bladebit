b256d0a319318946d044b38192dedc83dc0d3b9c
Harold B <harold@hbrenes.com>
2022-04-26 19:51:22 -0500

    Working on CI build stuff for macos and versioning

--- embed-version.sh
+++ embed-version.sh
@@ -13,10 +13,15 @@ git_commit=${version[4]}
 echo "Version: $ver_maj.$ver_min.$ver_rev$ver_suffix"
 echo "Commit : $git_commit"
 
+sed_inline=
+if [[ $OSTYPE == 'darwin'* ]]; then
+  sed_inline="\'\'"
+fi
+
 version_header='src/Version.h'
-sed -i -E -r "s/(#define BLADEBIT_VERSION_MAJ\s+)([0-9]+)/\1$ver_maj/g" $version_header
-sed -i -E -r "s/(#define BLADEBIT_VERSION_MIN\s+)([0-9]+)/\1$ver_min/g" $version_header
-sed -i -E -r "s/(#define BLADEBIT_VERSION_REV\s+)([0-9]+)/\1$ver_rev/g" $version_header
-sed -i -E -r "s/(#define BLADEBIT_VERSION_SUFFIX\s+)(\".*\")/\1\"$ver_suffix\"/g" $version_header
-sed -i -E -r "s/(#define BLADEBIT_GIT_COMMIT\s+)(\".*\")/\1\"$git_commit\"/g" $version_header
+sed -i ${sed_inline} -E -r "s/([[:space:]]*#define[[:space:]]+BLADEBIT_VERSION_MAJ[[:space:]]+)([0-9]+)/\1$ver_maj/g" $version_header
+sed -i ${sed_inline} -E -r "s/([[:space:]]*#define[[:space:]]+BLADEBIT_VERSION_MIN[[:space:]]+)([0-9]+)/\1$ver_min/g" $version_header
+sed -i ${sed_inline} -E -r "s/([[:space:]]*#define[[:space:]]+BLADEBIT_VERSION_REV[[:space:]]+)([0-9]+)/\1$ver_rev/g" $version_header
+sed -i ${sed_inline} -E -r "s/([[:space:]]*#define[[:space:]]+BLADEBIT_VERSION_SUFFIX[[:space:]]+)(\".*\")/\1\"$ver_suffix\"/g" $version_header
+sed -i ${sed_inline} -E -r "s/([[:space:]]*#define[[:space:]]+BLADEBIT_GIT_COMMIT[[:space:]]+)(\".*\")/\1\"$git_commit\"/g" $version_header
 
