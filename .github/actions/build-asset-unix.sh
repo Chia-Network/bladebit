1f1fe1189eda93d8ed64e0cae0ed9c7c2a96791a
Kyle Altendorf <sda@fstab.net>
2022-11-03 16:08:40 -0400

    _lots_

--- .github/actions/build-asset-unix.sh
+++ .github/actions/build-asset-unix.sh
@@ -34,21 +34,5 @@ if [[ "$bb_version" != "$BB_VERSION" ]]; then
     exit 1
 fi
 
-tar --version
-ls -la bladebit
-python3 -c 'f = open("bladebit", "rb"); contents = f.read(); print("zero count:", contents.count(b"\x00"))'
-tar -czvf $BB_ARTIFACT_NAME bladebit
-ls -la $BB_ARTIFACT_NAME
 mkdir ../bin
 cp bladebit ../bin/
-mkdir tmp1
-cd tmp1
-tar -xvf ../$BB_ARTIFACT_NAME
-python3 -c 'f = open("bladebit", "rb"); contents = f.read(); print("zero count:", contents.count(b"\x00"))'
-cd ..
-mv $BB_ARTIFACT_NAME ../bin/
-ls -la ../bin/
-mkdir tmp2
-cd tmp2
-tar -xvf ../../bin/$BB_ARTIFACT_NAME
-python3 -c 'f = open("bladebit", "rb"); contents = f.read(); print("zero count:", contents.count(b"\x00"))'
