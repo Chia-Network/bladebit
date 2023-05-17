35a1d9d52a56f0dbc743153eefb234a5f21a4722
William Allen <wallentx@users.noreply.github.com>
2023-05-16 20:53:38 -0500

    fix embed

--- embed-version.sh
+++ embed-version.sh
@@ -1,9 +1,17 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 set -eo pipefail
 if [[ $RUNNER_DEBUG = 1 ]]; then
   set -x
 fi
 cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# Check for necessary commands
+for cmd in sed; do
+  if ! command -v $cmd &>/dev/null; then
+    echo "$cmd could not be found"
+    exit 1
+  fi
+done
 
 # version=($(./extract-version.sh))
 . ./extract-version.sh
@@ -17,14 +25,16 @@ git_commit=$bb_git_commit
 echo "Version: $ver_maj.$ver_min.$ver_rev$ver_suffix"
 echo "Commit : $git_commit"
 
-sed_inline=
-if [[ $OSTYPE == 'darwin'* ]]; then
-  sed_inline="\'\'"
-fi
-
 version_header='src/Version.h'
-sed -i ${sed_inline} -E -r "s/([[:space:]]*#define[[:space:]]+BLADEBIT_VERSION_MAJ[[:space:]]+)([0-9]+)/\1$ver_maj/g" $version_header
-sed -i ${sed_inline} -E -r "s/([[:space:]]*#define[[:space:]]+BLADEBIT_VERSION_MIN[[:space:]]+)([0-9]+)/\1$ver_min/g" $version_header
-sed -i ${sed_inline} -E -r "s/([[:space:]]*#define[[:space:]]+BLADEBIT_VERSION_REV[[:space:]]+)([0-9]+)/\1$ver_rev/g" $version_header
-sed -i ${sed_inline} -E -r "s/([[:space:]]*#define[[:space:]]+BLADEBIT_VERSION_SUFFIX[[:space:]]+)(\".*\")/\1\"$ver_suffix\"/g" $version_header
-sed -i ${sed_inline} -E -r "s/([[:space:]]*#define[[:space:]]+BLADEBIT_GIT_COMMIT[[:space:]]+)(\".*\")/\1\"$git_commit\"/g" $version_header
+
+# Define a function for the repeated sed commands
+update_version_header() {
+  local define=$1 value=$2
+  sed -i'.bak' -E "s/([[:space:]]*#define[[:space:]]+$define[[:space:]]+)([0-9]+|\".*\")/\1$value/g" "$version_header" && rm "${version_header}.bak"
+}
+
+update_version_header "BLADEBIT_VERSION_MAJ" $ver_maj
+update_version_header "BLADEBIT_VERSION_MIN" $ver_min
+update_version_header "BLADEBIT_VERSION_REV" $ver_rev
+update_version_header "BLADEBIT_VERSION_SUFFIX" "\"$ver_suffix\""
+update_version_header "BLADEBIT_GIT_COMMIT" "\"$git_commit\""
