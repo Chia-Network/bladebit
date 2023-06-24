bb9a06bf180f0552d34fd024e8fea06a07038051
William Allen <wallentx@users.noreply.github.com>
2023-06-24 12:39:42 -0500

    Update FindCatch2.cmake
    Fix for gcc13

--- cmake_modules/FindCatch2.cmake
+++ cmake_modules/FindCatch2.cmake
@@ -3,7 +3,7 @@ Include(FetchContent)
 FetchContent_Declare(
         Catch2
         GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-        GIT_TAG        v3.0.0-preview4
+        GIT_TAG        v3.3.2
 )
 
 FetchContent_MakeAvailable(Catch2)
