a8e0d0ef0c746ff7abc6fa7ef027c39f205ee654
wallentx <william.allentx@gmail.com>
2023-08-31 00:16:39 -0500

    update get-version.sh

--- .github/actions/get-version.sh
+++ .github/actions/get-version.sh
@@ -1,36 +1,32 @@
-#! /usr/bin/env bash
-# NOTE: This is meant to be run from the repo root dir
-#
+#!/usr/bin/env bash
 set -eo pipefail
 
 os=$1
 arch=$2
 
-shift $#
+# Reading the VERSION file
+readarray -t lines < VERSION
 
-# version_cmp=($(./extract-version.sh))
-. ./extract-version.sh
+ver_maj=$(echo "${lines[0]}" | cut -d'.' -f1)
+ver_min=$(echo "${lines[0]}" | cut -d'.' -f2)
+ver_rev=$(echo "${lines[0]}" | cut -d'.' -f3)
 
-ver_maj=$bb_ver_maj
-ver_min=$bb_ver_min
-ver_rev=$bb_ver_rev
-ver_suffix=$bb_version_suffix
+# Extracting suffix from VERSION file
+ver_suffix=${lines[1]}
 
+# Suffix logic based on CI environment variable
+if [ -z "$CI" ]; then
+    ver_suffix="-dev"
+fi
+
+# Forming the full version string
 version="${ver_maj}.${ver_min}.${ver_rev}${ver_suffix}"
 
-# echo "Ref name: '$GITHUB_REF_NAME'"
-# if [[ "$GITHUB_REF_NAME" != "master" ]]; then
-#     suffix="-${GITHUB_REF_NAME}"
-# fi
-
 ext="tar.gz"
-
 if [[ "$os" == "windows" ]]; then
     ext="zip"
 fi
 
-echo "BB_VERSION=$version" >> $GITHUB_ENV
-echo "BB_ARTIFACT_NAME=bladebit-v${version}-${os}-${arch}.${ext}" >> $GITHUB_ENV
-echo "BB_ARTIFACT_NAME_CUDA=bladebit-cuda-v${version}-${os}-${arch}.${ext}" >> $GITHUB_ENV
-
-
+echo "BB_VERSION=$version" >> "$GITHUB_ENV"
+echo "BB_ARTIFACT_NAME=bladebit-v${version}-${os}-${arch}.${ext}" >> "$GITHUB_ENV"
+echo "BB_ARTIFACT_NAME_CUDA=bladebit-cuda-v${version}-${os}-${arch}.${ext}" >> "$GITHUB_ENV"
